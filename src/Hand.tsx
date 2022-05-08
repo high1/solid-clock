776cbe15935405eb66ef7fe0b5e53c842b17c279
high1 <high1@mailfence.com>
2022-05-08 21:21:27 +0200

    Updated the implementation

--- src/Hand.tsx
+++ src/Hand.tsx
@@ -4,10 +4,8 @@ type HandProps = { rotate: Accessor<string>, class: string, length: number, widt
 
 export const Hand: Component<HandProps> = ({ rotate, length, width, fixed, ...rest }) => (
   <line
-    x1={fixed ? 195 - length : 100}
-    y1={100}
-    x2={fixed ? 195: 100}
-    y2={100 - (fixed ? 0 : length)}
+    {...fixed && { y1: length - 95 }}
+    y2={-(fixed ? 95 : length)}
     stroke="currentColor"
     stroke-width={width}
     stroke-linecap="round"
--- src/Lines.tsx
+++ src/Lines.tsx
@@ -3,9 +3,9 @@ import type { Component } from 'solid-js';
 
 type LinesProps = { numberOfLines: number, class: string, length: number, width: number };
 
-const rotate = (index: number, length: number) => () => `rotate(${(360 * index) / length} 100 100)`;
+const rotate = (index: number, length: number) => () => `rotate(${(360 * index) / length})`;
 
-export const Lines: Component<LinesProps> = ({ numberOfLines, ...rest} ) => (
+export const Lines: Component<LinesProps> = ({ numberOfLines, ...rest}) => (
   Array.from({ length: numberOfLines }).map((_, index) =>
     <Hand rotate={rotate(index, numberOfLines)} {...rest} fixed />
   )
