b6d06033a5c6778a56cc7f2651ac8969cd02a659
high1 <high1@mailfence.com>
2022-10-16 18:11:33 +0200

    fix: removed will change, code unification
    - removed will change optimization
    - do not split class props on ClockHand, instead set stroke-linecap
    directly to shorten the code

--- src/ClockHand.tsx
+++ src/ClockHand.tsx
@@ -8,18 +8,16 @@ type ClockHandProps = {
 
 export const ClockHand = (props: ClockHandProps) => {
   const props_ = mergeProps({ length: 0, limit: 94, class: '' }, props);
-  const [local, rest] = splitProps(props_, [
-    'class',
-    'length',
-    'limit',
-    'stationary',
-  ]);
+  const [local, rest] = splitProps(
+    mergeProps({ length: 0, limit: 94, class: '' }, props),
+    ['length', 'limit', 'stationary']
+  );
 
   return (
     <line
-      class={`stroke-cap-round ${local.class}`}
       y1={local.stationary ? local.length - local.limit : undefined}
       y2={-(local.stationary ? local.limit : local.length)}
+      stroke-linecap="round"
       {...rest}
     />
   );
