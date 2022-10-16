84425f85405b73a7df060bacbe6319338dfd8fcc
high1 <high1@users.noreply.github.com>
2022-10-16 18:14:54 +0200

    fix:removed will change (#32)
    * fix: removed will change, code unification
    
    - removed will change optimization
    - do not split class props on ClockHand, instead set stroke-linecap
    directly to shorten the code
    
    * fix: lint ClockHand

--- src/ClockFace.tsx
+++ src/ClockFace.tsx
@@ -52,7 +52,7 @@ export const ClockFace = () => {
         <g class="translate-100px">
           <ClockHand
             transform={subsecond()}
-            class="stroke-neutral-200 @dark:stroke-neutral-600 stroke-width-5 will-change-transform"
+            class="stroke-neutral-200 @dark:stroke-neutral-600 stroke-width-5"
             length={83}
           />
           <ClockHand
