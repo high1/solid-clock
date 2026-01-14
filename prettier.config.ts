76c5e4fd76f65b3d5c85ce22761085ac7d915428
high1 <high1@mailfence.com>
2026-01-14 01:00:28 +0100

    fix(config): Small ESLint & Prettier fixes

--- prettier.config.ts
+++ prettier.config.ts
@@ -3,4 +3,5 @@ import { type Config } from 'prettier';
 export default {
   plugins: ['prettier-plugin-tailwindcss'],
   singleQuote: true,
+  tailwindStylesheet: 'src/index.css',
 } satisfies Config;
