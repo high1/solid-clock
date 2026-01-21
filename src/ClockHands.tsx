ec0dc7c1e5aeab54ddaaa2aba9b65beb75c1ffb6
high1 <high1@users.noreply.github.com>
2026-01-21 02:38:55 +0100

    feat: moved time signals to a separate file (#278)

--- src/ClockHands.tsx
+++ src/ClockHands.tsx
@@ -1,22 +1,12 @@
-import { createSignal, onCleanup } from 'solid-js';
+import { onCleanup } from 'solid-js';
 
 import { ClockLine as ClockHand } from '@/ClockLine';
-import { hours, rotate, seconds } from '@/common';
+import { time } from '@/time';
 import { getTestId } from '@/utilities';
 
-const getSecondsSinceMidnight = (): number =>
-  (Date.now() - new Date().setHours(0, 0, 0, 0)) / 1000;
-
 export const ClockHands = () => {
-  const [time, setTime] = createSignal(getSecondsSinceMidnight());
-
-  const subsecond = () => rotate(time() % 1, 0);
-  const second = () => rotate((time() % seconds) / seconds);
-  const minute = () => rotate(((time() / seconds) % seconds) / seconds);
-  const hour = () => rotate(((time() / seconds ** 2) % hours) / hours);
-
   let frame = requestAnimationFrame(function loop() {
-    setTime(getSecondsSinceMidnight());
+    time.update();
     frame = requestAnimationFrame(loop);
   });
 
@@ -30,22 +20,22 @@ export const ClockHands = () => {
         class="stroke-zinc-200 stroke-3 dark:stroke-zinc-600"
         data-testid={getTestId('subsecond')}
         length={82}
-        transform={subsecond()}
+        transform={time.milisecond}
       />
       <ClockHand
         class="stroke-zinc-600 stroke-4 dark:stroke-zinc-200"
         length={46}
-        transform={hour()}
+        transform={time.hour}
       />
       <ClockHand
         class="stroke-zinc-400 stroke-3"
         length={64}
-        transform={minute()}
+        transform={time.minute}
       />
       <ClockHand
         class="stroke-solid-light stroke-2 dark:stroke-solid"
         length={76}
-        transform={second()}
+        transform={time.second}
       />
     </>
   );
