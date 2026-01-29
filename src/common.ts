fa462bac425ccfd1e4d9d05a1725fdb26646c00f
high1 <high1@users.noreply.github.com>
2026-01-30 00:47:38 +0100

    fix(test): Cleanup, common export (#297)

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,4 @@
 export const seconds = 60;
 export const rotate = (rotate: number, fractionDigits = 1) =>
   `rotate(${(rotate * 360).toFixed(fractionDigits)})`;
+export const clockFaceId = 'clock-face';
