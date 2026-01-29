183e026b2793029c15e2d63f70c8807daa52b3f7
high1 <high1@users.noreply.github.com>
2026-01-29 10:42:53 +0100

    feat(test): Vitest browser mode, seperate test config for type safety (#292)
    * feat(test): Vitest browser mode, seperate test config for type safety
    
    * fix(test): Added playwright to install (#293)
    
    * fix(test): Added playwright to dependencies

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
-/// <reference types="@testing-library/jest-dom/vitest" />
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
