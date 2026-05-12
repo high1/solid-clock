16ef35ef3480430d11a2c6f89993ccd4dc7d32c1
high1 <high1@mailfence.com>
2026-05-12 11:31:42 +0200

    chore(test): moved vi mock up

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,10 @@
 import { render } from 'solid-js/web';
 import { describe, expect, test, vi } from 'vitest';
 
+vi.mock('solid-js/web', { spy: true });
+
 describe('index', () => {
   test('renders without crashing', async () => {
-    vi.mock('solid-js/web', { spy: true });
     await import('#src/index');
     expect(render).toHaveBeenCalledWith(expect.any(Function), document.body);
   });
